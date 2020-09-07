736c539a175f39ba494b859c239cca83fea71499
Sarah O'Brien <snobrien@uvm.edu>
2020-09-07 13:45:16 -0400

    step 48

--- leapInfiniteLoop.js
+++ leapInfiniteLoop.js
@@ -1,9 +1,12 @@
 var controllerOptions = {};
 
 var i = 0;
+var x = (window.innerWidth) / 2
+var y = (window.innerHeight) / 2
 Leap.loop(controllerOptions, function(frame)
 {
 
+    circle(x, y, 100)
     console.log(i)
 
 }

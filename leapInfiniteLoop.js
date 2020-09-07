06aaa2de89b8c983f14c168a52461e18bbd41e0b
Sarah O'Brien <snobrien@uvm.edu>
2020-09-07 15:51:48 -0400

    62 - refactoring part 1

--- leapInfiniteLoop.js
+++ leapInfiniteLoop.js
@@ -6,33 +6,30 @@ var y = (window.innerHeight) / 2;
 
 Leap.loop(controllerOptions, function(frame)
 {
-
+    HandleFrame(frame);
     //console.log(frame);
     //clear();
     //var z = (Math.floor(Math.random()* 7)) -1; //1 was too small to see a change - used 7
    // var j = (Math.floor(Math.random()* 7)) -1; //1 was too small to see a change - used 7
     //circle(x + z, y + j, 100);
     //console.log(i);
-    
-    if (frame.hands.length == 1 ){
-        var hand = frame.hands[0];
-        var fingers = hand.fingers;
-        //console.log(fingers);
-
-        for (var i = 0; i < hand.fingers.length; i++){
-            finger = hand.fingers[i];
-            //console.log(finger);
-            if (finger.type == 1){
-                console.log(finger);
-            }
-        }
-
-    
         
-
-    }
-
-    
     
 }
 );
+
+function HandleFrame (frame){
+    var hand;
+    if (frame.hands.length == 1 ){
+        var hand = frame.hands[0];
+        var fingers = hand.fingers;
+
+        for (var i = 0; i < hand.fingers.length; i++){
+            finger = hand.fingers[i];
+            if (finger.type == 1){
+                console.log(finger);
+            }
+        } 
+        
+    }
+}

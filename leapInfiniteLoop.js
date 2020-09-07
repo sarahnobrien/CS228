3f2c6d54ab32211b473c12cf01553195e26cfc26
Sarah O'Brien <snobrien@uvm.edu>
2020-09-07 13:51:39 -0400

    49 - jiggling circle

--- leapInfiniteLoop.js
+++ leapInfiniteLoop.js
@@ -3,10 +3,11 @@ var controllerOptions = {};
 var i = 0;
 var x = (window.innerWidth) / 2
 var y = (window.innerHeight) / 2
+
 Leap.loop(controllerOptions, function(frame)
 {
-
-    circle(x, y, 100)
+    var z = (Math.floor(Math.random()* 1)) - 1
+    circle(x + z, y, 100)
     console.log(i)
 
 }

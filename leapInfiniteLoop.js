89462c58481215f2ca6c6a0851c020ad0e9c6f5b
Sarah O'Brien <snobrien@uvm.edu>
2020-09-07 14:41:30 -0400

    step 57 - hand

--- leapInfiniteLoop.js
+++ leapInfiniteLoop.js
@@ -1,14 +1,21 @@
 var controllerOptions = {};
 
 var i = 0;
-var x = (window.innerWidth) / 2
-var y = (window.innerHeight) / 2
+var x = (window.innerWidth) / 2;
+var y = (window.innerHeight) / 2;
 
 Leap.loop(controllerOptions, function(frame)
 {
-    var z = (Math.floor(Math.random()* 1)) - 1
-    circle(x + z, y, 100)
-    console.log(i)
 
+    //console.log(frame);
+    //clear();
+    //var z = (Math.floor(Math.random()* 7)) -1; //1 was too small to see a change - used 7
+   // var j = (Math.floor(Math.random()* 7)) -1; //1 was too small to see a change - used 7
+    //circle(x + z, y + j, 100);
+    //console.log(i);
+    var hand = frame.hands[0];
+    if (frame.hands.length == 1 ){
+        console.log(hand);
+    }
 }
 );

3d060a5562f7ba850e903b63720fe4b9b7fa0497
Sarah O'Brien <snobrien@uvm.edu>
2020-09-13 17:12:57 -0400

    up to 25 - lightening lines

--- leapDrawLines.js
+++ leapDrawLines.js
@@ -1,9 +1,9 @@
 var controllerOptions = {};
 
 var i = 0;
-var x = {};
-var y = {};
-var z = {};
+var x = 0;
+var y = 0;
+var z = 0;
 var rawXMin = 1000;
 var rawXMax = 1;
 var rawYMin = 1000;
@@ -36,28 +36,12 @@ function HandleHand(hand){
 }
 
 function HandleFinger(finger){
-        if ( x < rawXMin){
-            rawXMin = x;
-            //console.log(rawXMin);
-        }
-
-        if (x > rawXMax){
-            rawXMax = x;
-            //console.log(rawXMax);
-        }
-
-        if (z < rawYMin){
-            rawYMin = z;
-        }
-
-        if (z > rawYMax){
-            rawYMax = z;
-        }
+/**
 
         x = finger.tipPosition[0];
         z = finger.tipPosition[1];
         y = finger.tipPosition[2];
-
+**/
     for (var i = 0; i < finger.bones.length; i++){
         bone = finger.bones[i];
         HandleBone(bone)
@@ -67,26 +51,71 @@ function HandleFinger(finger){
 }
 
 function HandleBone(bone){
-    oX = bone.prevJoint[0];
-    oZ = bone.prevJoint[1];
-    oY = bone.prevJoint[2];
+    xb = bone.prevJoint[0];
+    zb = bone.prevJoint[1];
+    yb = bone.prevJoint[2];
 
-    x = bone.nextJoint[0];
-    z = bone.nextJoint[1];
-    y = bone.nextJoint[2];
+    xt = bone.nextJoint[0];
+    zt = bone.nextJoint[1];
+    yt = bone.nextJoint[2];
 
-    oZ = -oZ + (window.innerHeight);
-    z = -z + (window.innerHeight);
+
+
+
+    [xb,zb] = TransformCoordinates(xb,zb); // base
+    [xt,zt] = TransformCoordinates(xt,zt); // tip
+
+    zb = -zb + (window.innerHeight);
+    zt = -zt + (window.innerHeight);
+
+    if (bone.type == 0) {
+        strokeWeight(10);
+        stroke('rgb(220,220,220)');
+        line(xt,zt,xb,zb);
+    }
+
+    if (bone.type == 1){
+        strokeWeight(7);
+        stroke('rgb(192,192,192)');
+        line(xt,zt,xb,zb);
+    }
+    if (bone.type == 2){
+        strokeWeight(4);
+        stroke('rgb(128,128,128)');
+        line(xt,zt,xb,zb);
+    }
+
+    else if (bone.type == 4){
+        strokeWeight(2);
+        stroke('rgb(105,105,105)');
+        line(xt,zt,xb,zb);
+    }
+
+
+
+    //console.log(bone);
+}
+
+function TransformCoordinates(x,z){
+    if ( x < rawXMin){
+        rawXMin = x;
+        //console.log(rawXMin);
+    }
+    if (x > rawXMax){
+        rawXMax = x;
+        //console.log(rawXMax);
+    }
+    if (z < rawYMin){
+        rawYMin = z;
+    }
+    if (z > rawYMax){
+        rawYMax = z;
+    }
 
     oldXRange = (rawXMax - rawXMin);
     oldYRange = (rawYMax - rawYMin);
+
     newXValue = (((x - rawXMin) * window.innerWidth) / oldXRange) + 0;
     newYValue = (((z - rawYMin) * window.innerHeight) / oldYRange) + 0;
-
-    newOXValue = (((oX - rawXMin) * window.innerWidth) / oldXRange) + 0;
-    newOYValue = (((oZ - rawYMin) * window.innerHeight) / oldYRange) + 0;
-
-    //circle(newXValue,newYValue,50);
-    line(newXValue,newYValue, newOXValue, newOYValue);
-    console.log(bone);
+    return[newXValue,newYValue]
 }

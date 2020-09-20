f1b45e59fce1725d59cd36c30aa93921ad270d92
Sarah O'Brien <snobrien@uvm.edu>
2020-09-20 15:55:37 -0400

    up to step 23

--- Record.js
+++ Record.js
@@ -9,37 +9,50 @@ var rawXMax = 1;
 var rawYMin = 1000;
 var rawYMax = 1;
 
+var previousNumHands = 0;
+var currentNumHands = 0;
 
+var OneFrameOfData = nj.zeros([5,4]);
 
 Leap.loop(controllerOptions, function(frame)
     {
+        console.log(OneFrameOfData.toString())
+
+        currentNumHands = frame.hands.length;
+        console.log(previousNumHands);
+        console.log(currentNumHands);
         clear();
         HandleFrame(frame);
+        if (previousNumHands == 2 && currentNumHands == 1){
+            RecordData();
+        }
+        previousNumHands = currentNumHands;
+
     }
 );
 
 function HandleFrame (frame){
     var hand;
-    if (frame.hands.length == 1 ){
+    if (frame.hands.length == 1 || frame.hands.length == 2){
         var hand = frame.hands[0];
         var fingers = hand.fingers;
-        HandleHand(hand)
+        HandleHand(hand, frame)
     }
 
 }
 
-function HandleHand(hand){
+function HandleHand(hand, frame){
     for (var j = 3; j >= 0; j--){
 
         for (var i = 0; i < hand.fingers.length; i++){
             finger = hand.fingers[i];
-            HandleBone(finger.bones[j])
+            HandleBone(finger.bones[j], frame, i)
         }
 
     }
 }
 
-function HandleFinger(finger){
+function HandleFinger(finger, frame){
 /**
 
         x = finger.tipPosition[0];
@@ -48,13 +61,14 @@ function HandleFinger(finger){
 **/
     for (var i = 0; i < finger.bones.length; i++){
         bone = finger.bones[i];
-        HandleBone(bone)
+        HandleBone(bone,frame)
     }
 
 
 }
 
-function HandleBone(bone){
+function HandleBone(bone,frame,fingerIndex){
+
     xb = bone.prevJoint[0];
     zb = bone.prevJoint[1];
     yb = bone.prevJoint[2];
@@ -63,7 +77,8 @@ function HandleBone(bone){
     zt = bone.nextJoint[1];
     yt = bone.nextJoint[2];
 
-
+    sumFinger = xb + zb + yb + xt + zt + yt;
+    OneFrameOfData.set(fingerIndex, sumFinger);
 
 
     [xb,zb] = TransformCoordinates(xb,zb); // base
@@ -72,32 +87,55 @@ function HandleBone(bone){
     zb = -zb + (window.innerHeight);
     zt = -zt + (window.innerHeight);
 
-    if (bone.type == 0) {
-        strokeWeight(10);
-        stroke('rgb(220,220,220)');
-        line(xt,zt,xb,zb);
+
+
+    if (frame.hands.length == 1) {
+
+        if (bone.type == 0) {
+            strokeWeight(11);
+            stroke('rgb(0,220,0)');
+            line(xt, zt, xb, zb);
+        }
+
+        if (bone.type == 1) {
+            strokeWeight(8);
+            stroke('rgb(0,140,0)');
+            line(xt, zt, xb, zb);
+        }
+        if (bone.type == 2) {
+            strokeWeight(6);
+            stroke('rgb(0,100,0)');
+            line(xt, zt, xb, zb);
+        } else if (bone.type == 3) {
+            strokeWeight(4);
+            stroke('rgb(0,50,0)');
+            line(xt, zt, xb, zb);
+        }
+    }
+    else{
+        if (bone.type == 0) {
+            strokeWeight(11);
+            stroke('rgb(220,0,0)');
+            line(xt, zt, xb, zb);
+        }
+
+        if (bone.type == 1) {
+            strokeWeight(8);
+            stroke('rgb(140,0,0)');
+            line(xt, zt, xb, zb);
+        }
+        if (bone.type == 2) {
+            strokeWeight(6);
+            stroke('rgb(100,0,0)');
+            line(xt, zt, xb, zb);
+        } else if (bone.type == 3) {
+            strokeWeight(4);
+            stroke('rgb(50,0,0)');
+            line(xt, zt, xb, zb);
+        }
     }
 
-    if (bone.type == 1){
-        strokeWeight(7);
-        stroke('rgb(192,192,192)');
-        line(xt,zt,xb,zb);
-    }
-    if (bone.type == 2){
-        strokeWeight(4);
-        stroke('rgb(128,128,128)');
-        line(xt,zt,xb,zb);
-    }
 
-    else if (bone.type == 3){
-        strokeWeight(2);
-        stroke('rgb(105,105,105)');
-        line(xt,zt,xb,zb);
-    }
-
-
-
-    //console.log(bone);
 }
 
 function TransformCoordinates(x,z){
@@ -122,4 +160,8 @@ function TransformCoordinates(x,z){
     newXValue = (((x - rawXMin) * window.innerWidth) / oldXRange) + 0;
     newYValue = (((z - rawYMin) * window.innerHeight) / oldYRange) + 0;
     return[newXValue,newYValue]
+}
+
+function RecordData(){
+    background(51);
 }

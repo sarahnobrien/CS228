1ea9076f95ee6ce5003bba0b62f2d9672be09801
Sarah O'Brien <snobrien@uvm.edu>
2020-09-27 19:48:52 -0400

    creating the training and testing phases

--- Predict.js
+++ Predict.js
@@ -1,6 +1,20 @@
-function draw()
-{
-    clear();
+var trainingCompleted  = false;
+
+function Train(){
+    console.log("I am being trained");
+    trainingCompleted = true;
 }
 
+function Test(){
+    console.log("I am going to do more than just train");
+}
+
+function draw() {
+    clear();
+    if (trainingCompleted == false){
+        Train();
+    }
+
+    Test();
+}
 

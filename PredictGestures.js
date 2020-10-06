f1f67cb3a82e18fd5c68cdd16889a3cb184a7295
Sarah O'Brien <snobrien@uvm.edu>
2020-10-05 20:26:30 -0400

    Testing the kNN on your data.

--- PredictGestures.js
+++ PredictGestures.js
@@ -2,11 +2,18 @@ const knnClassifier = ml5.KNNClassifier();
 
 
 var trainingCompleted  = false;
+var testingSampleIndex = 0;
+var numSamples = train0.shape[0];
+var predictedClassLabels = nj.zeros([numSamples]);
 
 
 function GotResults(err, result){
-
-
+    predictedClassLabels[testingSampleIndex] = parseInt(result.label);
+    testingSampleIndex++ ;
+    if (testingSampleIndex > numSamples){
+        testingSampleIndex = 0;
+    }
+    console.log(parseInt(result.label))
 
 
 
@@ -16,16 +23,23 @@ function Train(){
         var features = train0.pick(null,null,null,i);
         features = features.reshape(120).tolist();
         knnClassifier.addExample(features, 0);
-        console.log(features.toString());
+
+        var features = train1.pick(null,null,null,i);
+        features = features.reshape(120).tolist();
+        knnClassifier.addExample(features, 0);
     }
     trainingCompleted = true;
 
 }
 
 function Test(){
-
-
-
+    for (var i = 0; i < train0.shape[3]; i++ ) {
+        var currentTestingSample = test.pick(null, null, null, i);
+        currentTestingSample = currentTestingSample.reshape(120).tolist()
+        //var currentLabel = test.get(testingSampleIndex, -1);
+        var predictedLabel = knnClassifier.classify(currentTestingSample, GotResults);
+        console.log(currentTestingSample );
+    }
 }
 
 function DrawCircles(){
@@ -37,10 +51,11 @@ function DrawCircles(){
 
 function draw() {
     clear();
-    if (trainingCompleted == false){
+    if (trainingCompleted == false) {
         Train();
     }
 
+    Test()
 
 
 }

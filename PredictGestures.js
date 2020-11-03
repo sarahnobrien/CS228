27246f6e98994c93071e08b0106b6048c9f585cd
Sarah O'Brien <snobrien@uvm.edu>
2020-11-02 21:02:38 -0500

    fin of deliv 8

--- PredictGestures.js
+++ PredictGestures.js
@@ -530,14 +530,14 @@ function DrawImageToHelpUserPutTheirHandOverTheDevice(){
 function SignIn(){
     //console.log("hey this function was just called!")
 
-    username = document.getElementById("username").value;
-    //console.log(username)
+    username = document.getElementById('username').value;
     var list = document.getElementById('users');
     if (IsNewUser(username,list)) {
-        var item = document.createElement('li');
-        item.innerHTML = String(username);
-        list.appendChild(item);
+        CreateNewUser(username,list);
+    }else {
+        CreateSignInItem(username,list);
     }
+    console.log("Signed In - " + username);
     console.log(list.innerHTML);
     return false;
 
@@ -555,10 +555,31 @@ function IsNewUser(username,list){
             console.log(users[i].innerHTML);
         }
     }
+
     return usernameFound == false;
 
-
 }
+
+function CreateNewUser(username,list)
+{
+    var item = document.createElement('li');
+    item.id = String(username) + "_name";
+    item.innerHTML = String(username);
+    list.appendChild(item);
+    item = document.createElement('li');
+    item.id = String(username) + "_signins";
+    item.innerHTML = 1;
+    list.appendChild(item);
+}
+
+
+function CreateSignInItem(username,list)
+{
+    var ID = String(username) + "_signins";
+    var listItem = document.getElementById(ID);
+    listItem.innerHTML = parseInt(listItem.innerHTML) + 1;
+}
+
 
 
 Leap.loop(controllerOptions, function(frame) {
